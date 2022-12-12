13c89804a390a0fba7338c2e82985bede5a92c46
VyacheslavTsukanov <vtsukanoff@yandex.ru>
2022-12-12 22:17:57 +0400

    Validation of forms

--- ASPTheDepartment.Models/Employee.cs
+++ ASPTheDepartment.Models/Employee.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace ASPTheDepartment.Models
 {
     public class Employee
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "The name field cannot be null!!!")]
         public string Name { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$", ErrorMessage = "Please, enter a Valid Email.")]
         public string Email { get; set; }
         public string PhotoPath { get; set; }
         public Dept? Department { get; set; }
--- ASPTheDepartment/Pages/Employees/Edit.cshtml.cs
+++ ASPTheDepartment/Pages/Employees/Edit.cshtml.cs
@@ -22,6 +22,7 @@ namespace ASPTheDepartment.Pages
             _webHostEnvironment = webHostEnvironment;
         }
 
+        [BindProperty]
         public Employee Employee { get; set; }
 
         [BindProperty]
@@ -41,22 +42,28 @@ namespace ASPTheDepartment.Pages
             return Page();
         }
 
-        public IActionResult OnPost(Employee employee)
+        public IActionResult OnPost()
         {
-            if(Photo != null)
+            if (ModelState.IsValid)
             {
-                if(employee.PhotoPath != null)
+                if (Photo != null)
                 {
-                    string filePath = Path.Combine(_webHostEnvironment.WebRootPath, "images", employee.PhotoPath);
-                    System.IO.File.Delete(filePath);
+                    if (Employee.PhotoPath != null)
+                    {
+                        string filePath = Path.Combine(_webHostEnvironment.WebRootPath, "images", Employee.PhotoPath);
+                        System.IO.File.Delete(filePath);
+                    }
+
+                    Employee.PhotoPath = ProcessUploadedFile();
                 }
 
-                employee.PhotoPath = ProcessUploadedFile();
+                Employee = _employeeRepository.Update(Employee);
+                TempData["SeccessMessage"] = $"Update {Employee.Name} successful!";
+                return RedirectToPage("Employees");
             }
 
-            Employee = _employeeRepository.Update(employee);
-            TempData["SeccessMessage"] = $"Update {Employee.Name} successful!";
-            return RedirectToPage("Employees");
+            return Page();
+           
         }
 
         public void OnPostUpdateNotificationPreferences(int id)
